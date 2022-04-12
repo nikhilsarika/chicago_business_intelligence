303204019e7a8a9865e0d0a5ea2f3e4d0eb169f7
nikhilsarika <n2kh2l@gmail.com>
2022-04-12 00:48:45 -0500

    modified cloud build structure 23

--- main.go
+++ main.go
@@ -241,7 +241,7 @@ func GetTaxiTrips(db *sql.DB) {
 	// Get your geocoder.ApiKey from here :
 	// https://developers.google.com/maps/documentation/geocoding/get-api-key?authuser=2
 
-	geocoder.ApiKey = "ADD-YOUR-API-KEY-HERE"
+	geocoder.ApiKey = "AIzaSyDDQqrB28SFOze44xgH0JwSO3ZNg49xlJo"
 
 	
 
