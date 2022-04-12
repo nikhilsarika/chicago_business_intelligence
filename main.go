4913f5afe3cd87d5ae35cbad2535c6ceee882c40
nikhilsarika <n2kh2l@gmail.com>
2022-04-11 22:49:09 -0500

    modified cloud build structure 19

--- main.go
+++ main.go
@@ -171,11 +171,11 @@ func main() {
 	// OPTION 1 - Postgress application running on localhost
 	//db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=localhost sslmode=disable port = 5432"
 	
-	//db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=localhost sslmode=disable port = 5432"
+	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=localhost sslmode=disable port = 5432"
 
 	// OPTION 2
 	// Docker container for the Postgres microservice - uncomment when deploy with host.docker.internal
-	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=host.docker.internal sslmode=disable port = 5433"
+	//db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=host.docker.internal sslmode=disable port = 5433"
 
 
 	// OPTION 3
