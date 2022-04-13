9d9a8ff454c18294e3a10c27d957a91ed1604c48
nikhilsarika <n2kh2l@gmail.com>
2022-04-13 14:49:00 -0500

    modified cloud build structure 37

--- main.go
+++ main.go
@@ -186,7 +186,7 @@ func main() {
 	
 	//Option 4
 	//Database application running on Google Cloud Platform. 
-	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/chicago-business-intelligence:us-central1:mypostgres sslmode=disable port = 5432"
+	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/chicago-business-intelligence:us-central1:mypostgres sslmode=disable"
 	
 
 	db, err := sql.Open("postgres", db_connection)
